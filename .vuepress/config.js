3b851bb69389a3f66539678c399b70d73581195b
EGOIST <0x142857@gmail.com>
2018-05-25 20:14:04 +0800

    add missing

---
+++ .vuepress/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: 'Ream',
+  description: 'Universal Vue.js Applications Made Simple',
+  serviceWorker: true,
+  themeConfig: {
+    repo: 'ream/ream',
+    docsRepo: 'ream/ream-website',
+    docsDir: 'docs',
+    editLinks: true,
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/introduction' }
+    ],
+    sidebar: [
+      {
+        title: 'Guide',
+        children: [
+          '/guide/installation',
+          '/guide/introduction',
+          '/guide/data-prefetching',
+          '/guide/custom-document',
+          '/guide/serve-public-files',
+          '/guide/manipulating-head',
+        ]
+      },
+      '/entry-file',
+      '/config',
+    ]
+  }
+}
